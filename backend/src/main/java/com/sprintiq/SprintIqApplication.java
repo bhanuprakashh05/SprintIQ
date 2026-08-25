51734076c15b291146c27e91639fd838b95a40dc
Bhanu Prakash <bhanuprakash1014gmail.com>
2026-08-25 09:59:44 +0530

    Initialize SprintIQ Spring Boot backend

---
+++ backend/src/main/java/com/sprintiq/SprintIqApplication.java
@@ -0,0 +1,12 @@
+package com.sprintiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintIqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintIqApplication.class, args);
+    }
+}
